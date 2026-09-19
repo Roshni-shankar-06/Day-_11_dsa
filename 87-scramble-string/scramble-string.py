83d2005ccb573d42728f62bcc1579559cf905f76
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-19 19:03:12 +0530

    Simplify isScramble function logic
    Remove unnecessary check for character count equality.

--- 87-scramble-string/scramble-string.py
+++ 87-scramble-string/scramble-string.py
@@ -3,7 +3,6 @@ class Solution:
   def isScramble(self, s1: str, s2: str) -> bool:
     if s1 == s2:
       return True
-    if collections.Counter(s1) != collections.Counter(s2):
-      return False
+
 
 
