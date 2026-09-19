59609253cb777ae911d6092a05feec421c0fb701
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-19 19:06:53 +0530

    Refactor minimumTotal function in triangle.cpp
    Removed unnecessary return statement and cleaned up code.

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -4,6 +4,4 @@ class Solution {
     for (int i = triangle.size() - 2; i >= 0; --i)
       for (int j = 0; j <= i; ++j)
         triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1]);
-    return triangle[0][0];
-  }
-};
+  
