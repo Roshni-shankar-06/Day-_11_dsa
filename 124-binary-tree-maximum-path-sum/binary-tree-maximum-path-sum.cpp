8676a29d165d3ea01136cd839f199f5467b7b2fb
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-19 19:05:44 +0530

    Remove maxPathSumDownFrom function declaration
    Removed the maxPathSumDownFrom function declaration and its comments.

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -6,8 +6,4 @@ class Solution {
     return ans;
   }
 
- private:
-  // Returns the maximum path sum starting from the current root, where
-  // root->val is always included.
-  int maxPathSumDownFrom(TreeNode* root, int& ans) {
-  
+ 
