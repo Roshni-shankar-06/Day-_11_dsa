aaa3168cfa53ffb2094c239fc37b77b634edde6a
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-19 19:04:41 +0530

    Remove clamp function for coordinate clamping
    Removed the clamp function used for bounding coordinates.

--- 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -2,8 +2,5 @@ class Solution {
  public:
   bool checkOverlap(int radius, int x_center, int y_center, int x1, int y1,
                     int x2, int y2) {
-    auto clamp = [&](int center, int mn, int mx) {
-      return max(mn, min(mx, center));
-    };
 
    
