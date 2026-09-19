51df2a75ff5d3b81e6e17216ddf76e4954e4e7a0
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-19 19:02:49 +0530

    Refactor isScramble function logic
    Removed redundant check for scrambled strings.

--- 87-scramble-string/scramble-string.py
+++ 87-scramble-string/scramble-string.py
@@ -9,6 +9,4 @@ class Solution:
     for i in range(1, len(s1)):
       if self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:]):
         return True
-      if (self.isScramble(s1[:i], s2[len(s2) - i:]) and
-              self.isScramble(s1[i:], s2[: len(s2) - i])):
-      
+  
